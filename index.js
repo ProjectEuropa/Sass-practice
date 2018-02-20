9844fcb6919e41955fb96638d8d64c862131791d
M2 <project-europa@excite.co.jp>
2018-02-20 14:54:11 +0900

    style:一時保存のためのコミット

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ $(function () {
     $('.slide-menu').hover(function () {
         $(this).children('ul').stop().slideToggle();
     });
+    $('.slick-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+    });
+    $('#calendar').fullCalendar();
+    document.getElementById("nowYear").innerText = new Date().getFullYear();
 });
