c4a44d29e2c91e1c0b1b8f632064fc1e9dda6cd2
M2 <project-europa@excite.co.jp>
2018-02-21 18:11:48 +0900

    style:写真を追加し、メニューバーをスマホ対応

--- index.js
+++ index.js
@@ -11,5 +11,34 @@ $(function () {
         arrows: false,
     });
     $('#calendar').fullCalendar();
+
+    $('#login-button').click(function () {
+        if (!$(this).hasClass('active')) {
+            $(this).addClass('active');
+        }
+        $('#register-button').removeClass('active');
+        $('#register').hide();
+        $('#login').show();
+    });
+    $('#register-button').click(function () {
+        if (!$(this).hasClass('active')) {
+            $(this).addClass('active');
+        }
+        $('#login-button').removeClass('active');
+        $('#login').hide();
+        $('#register').show();
+    });
+    $('#remove').click(function () {
+        setTimeout(function () {
+            $('.ggmap').remove();
+        }, 2000);
+    });
+    $('#show-smart-menu').click(function () {
+        $('.smart-show').show();
+    });
+    $('#hide-smart-menu').click(function () {
+        $('.smart-show').hide();
+    });
+
     document.getElementById("nowYear").innerText = new Date().getFullYear();
 });
