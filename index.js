2be746e9290bf390b7573ce1bf6ad27974f311d4
M2 <project-europa@excite.co.jp>
2018-02-22 14:56:41 +0900

    style:スマホ対応および微調整

--- index.js
+++ index.js
@@ -29,8 +29,17 @@ $(function () {
         $('#register').show();
     });
     $('#remove').click(function () {
+        $('#loading').show().activity({
+            segments: 12,
+            width: 12,
+            space: 6,
+            length: 28,
+            color: '#fff',
+            speed: 1.5
+        });
         setTimeout(function () {
             $('.ggmap').remove();
+            $('#loading').hide();
         }, 2000);
     });
     $('#show-smart-menu').click(function () {
@@ -38,6 +47,16 @@ $(function () {
     });
     $('#hide-smart-menu').click(function () {
         $('.smart-show').hide();
+    });
+    $('#smart-show-accordion').click(function () {
+        $('.smart-show-accordion-sub-menu').show();
+        $('#smart-hide-accordion').show();
+        $(this).hide();
+    });
+    $('#smart-hide-accordion').click(function () {
+        $('.smart-show-accordion-sub-menu').hide();
+        $('#smart-show-accordion').show();
+        $(this).hide();
     });
 
     document.getElementById("nowYear").innerText = new Date().getFullYear();
